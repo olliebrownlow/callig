2ea05fd8bd3c2313e1bf69d4f5a451c477a8aed5
Oliver Brownlow <olliebrownlow@gmail.com>
2024-04-25 15:57:29 +0000

    installs cypress, adds 2 passing navigation tests

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,31 @@
+describe("Navigation", () => {
+  it("should navigate to the contact page from a navlink", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
+
+    // Find a link with an href attribute containing "contact" and click it
+    cy.get('a[href*="contact"]').last().click();
+
+    // The new url should include "/contact"
+    cy.url().should("include", "/contact");
+
+    // The new page should contain an h1 with "Contact"
+    cy.get("h1").contains("Contact Details");
+  });
+
+  it("should navigate to the contact page from the burger menu", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
+    
+    cy.get('.bm-burger-button').click();
+
+    // Find a link with an href attribute containing "contact" and click it
+    cy.get('a[href*="contact"]').first().click();
+
+    // The new url should include "/contact"
+    cy.url().should("include", "/contact");
+
+    // The new page should contain an h1 with "Contact"
+    cy.get("h1").contains("Contact Details");
+  });
+});
